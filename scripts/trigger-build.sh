390544a4a8d8f433a7f492d1b90823934f3d9a72
Charles Moulliard <cmoulliard@redhat.com>
2023-06-02 11:50:49 +0200

    Added script fil to trigger a build
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

---
+++ scripts/trigger-build.sh
@@ -0,0 +1,12 @@
+BRANCH_NAME=pac-buildpack-build
+git checkout main
+git push -d origin ${BRANCH_NAME}
+git branch -D ${BRANCH_NAME}
+git checkout -b ${BRANCH_NAME}
+
+mkdir -p .tekton
+wget https://raw.githubusercontent.com/ch007m/tekton-pac/main/k8s/pipeline-new-buildpacks.yml -O .tekton/pipelinerun.yaml
+
+git add .tekton
+git commit -asm "This is a buildpacks build using TektonCI"
+git push --set-upstream origin ${BRANCH_NAME}
